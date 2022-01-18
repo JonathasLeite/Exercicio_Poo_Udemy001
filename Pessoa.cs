7c245dec1c525a71f16d141abbb5a008993c347c
JonathasLeite <jonathas.leite@yahoo.com.br>
2022-01-18 05:27:57 -0300

    Adicionar arquivos de projeto.

---
+++ Pessoa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace POO
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+
+       public void SalarioMaior()
+        {
+            var s1 = this.Salario;
+            var s2 = this.Salario;
+
+            if (s1 > s2)
+            {
+                Console.WriteLine($"Sálario maior: {Nome}");
+            }
+            else
+            {
+                Console.WriteLine($"Sálario maior: {Nome}");
+            }
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pe = new Pessoa();
+
+            var p1 = pe;
+            Console.WriteLine("Dados da Primeira pessoa:");
+            Console.Write("Nome:");            
+            p1.Nome = Console.ReadLine();
+            Console.Write("Sálario: ");
+            p1.Salario = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+
+            var p2 = pe;
+            Console.WriteLine("Dados da Segunda pessoa");
+            Console.Write("Nome:");
+            p2.Nome = Console.ReadLine();
+            Console.Write("Sálario:");
+            p2.Salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            pe.SalarioMaior();
+        }
+    }
+}
